73d3c53e09f342b755c5e053887f2c04d0158fae
misujumpei <misujumpei@gmail.com>
2026-01-22 11:29:07 +0900

    test: add regression tests for tsvstat

---
+++ test.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ng () {
+  echo "${1} line failed" 1>&2
+  res=1
+}
+
+res=0
+
+# NORMAL
+out=$(printf "a\t1\nb\t2\n" | ./tsvstat --col 2)
+[ "${out}" = 3.0 ] || ng "$LINENO"
+
+# MISSING COLUMN (exit 1)
+out=$(printf "a\t1\nb\n" | ./tsvstat --col 2)
+[ "$?" = 1 ] || ng "$LINENO"
+
+# MISSING OPTION (exit 2)
+out=$(printf "a\t1\n" | ./tsvstat)
+[ "$?" = 2 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
+
